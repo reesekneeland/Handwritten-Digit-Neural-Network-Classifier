850735a5351d259b0e7dc4855ceb7c95c52f7096
Reese Kneeland <kneel027@umn.edu>
2021-11-10 11:57:04 -0600

    adding base neural network structure code

---
+++ driver.py
@@ -0,0 +1,12 @@
+from typing_extensions import Annotated
+import neural_net as ann
+import numpy as np
+
+with np.load('mnist.npz') as data:
+    training_images = data['training_images']
+    training_labels = data['training_labels']
+
+layer_sizes = (784, 24, 10)
+
+net = ann.NeuralNetwork("layer_sizes")
+net.print_accuracy(training_images, training_labels)
