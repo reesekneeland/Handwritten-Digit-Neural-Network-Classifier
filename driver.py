4aa37f6fe228d240cd8a3702aea2ce653f4975a1
Reese Kneeland <kneel027@umn.edu>
2021-12-01 14:45:24 -0600

    updating driver framework and fixing dimension bugs

--- driver.py
+++ driver.py
@@ -1,12 +1,19 @@
 from typing_extensions import Annotated
 import neural_net as ann
 import numpy as np
+import matplotlib as plt
 
 with np.load('mnist.npz') as data:
     training_images = data['training_images']
     training_labels = data['training_labels']
+    test_images = data['test_images']
+    test_labels = data['test_labels']
+    validation_images = data['validation_images']
+    validation_labels = data['validation_labels']
 
 layer_sizes = (784, 128, 10)
 
 net = ann.NeuralNetwork(layer_sizes)
-net.print_accuracy(training_images, training_labels)
+net.train(training_images, training_labels, validation_images, validation_labels)
+net.test(test_images, test_labels)
+net.print_accuracy()
--- neural_net.py
+++ neural_net.py
@@ -6,60 +6,53 @@ class NeuralNetwork:
     def __init__(self, layer_sizes):
         weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:],layer_sizes[:-1])]
         self.weights = [np.random.standard_normal(s)/s[1]**.5 for s in weight_shapes]
-        self.biases = [np.zeros((s, 1)) for s in layer_sizes[1:]]
         self.learning_rate = 0.001
         self.epochs = 10000
         self.batch = 128
         self.losses,self.accuries,self.val_accuracies=[],[],[]
     #sigmoid function
-    def sigmoid(x):
+    def sigmoid(self, x):
         return 1/(np.exp(-x)+1)    
 
     #derivative of sigmoid
-    def d_sigmoid(x):
+    def d_sigmoid(self, x):
         return (np.exp(-x))/((np.exp(-x)+1)**2)
 
     #softmax function
-    def softmax(x):
+    def softmax(self, x):
         exp_element=np.exp(x-x.max())
         return exp_element/np.sum(exp_element,axis=0)
 
     #derivative of softmax
-    def d_softmax(x):
+    def d_softmax(self, x):
         exp_element=np.exp(x-x.max())
         return exp_element/np.sum(exp_element,axis=0)*(1-exp_element/np.sum(exp_element,axis=0))
 
     #forward and backward pass
-    def forward_backward_pass(x,y):
-        targets = np.zeros((len(y),10), np.float32)
-        targets[range(targets.shape[0]),y] = 1
-    
-        
-        x_l1=x.dot(l1)
-        x_sigmoid=sigmoid(x_l1)
-        x_l2=x_sigmoid.dot(l2)
-        out=softmax(x_l2)
-    
-    
-        error=2*(out-targets)/out.shape[0]*d_softmax(x_l2)
+    def forward_backward_pass(self, x,y):
+        targets = y.reshape((128, 10))
+        x_l1=x.dot(self.weights[0].T)
+        x_sigmoid=self.sigmoid(x_l1)
+        x_l2=x_sigmoid.dot(self.weights[1].T)
+        out=self.softmax(x_l2)
+        error=2*(out-targets)/out.shape[0]*(self.d_softmax(x_l2))
         update_l2=x_sigmoid.T@error
         
         
-        error=((l2).dot(error.T)).T*d_sigmoid(x_l1)
+        error=((self.weights[1].T).dot(error.T)).T*self.d_sigmoid(x_l1)
         update_l1=x.T@error
 
-        self.weights[0] = self.learning_rate * update_l1
-        self.weights[1] = self.learning_rate * update_l2
-        return out
+        
+        return out, update_l1, update_l2
 
-    def train(self, X_train, Y_train):
+    def train(self, X_train, Y_train, X_val, Y_val):
         for i in range(self.epochs):
             #randomize and create batches
             sample=np.random.randint(0,X_train.shape[0],size=(self.batch))
             x=X_train[sample].reshape((-1,28*28))
             y=Y_train[sample]
 
-            out = forward_backward_pass(x,y)   
+            out, update_l1, update_l2 = NeuralNetwork.forward_backward_pass(self, x,y)   
             category=np.argmax(out,axis=1)
             
             accuracy=(category==y).mean()
@@ -68,28 +61,25 @@ class NeuralNetwork:
             loss=((category-y)**2).mean()
             self.losses.append(loss.item())
 
+            self.weights[0] = self.weights[0] - self.learning_rate * update_l1.T
+            self.weights[1] = self.weights[1] - self.learning_rate * update_l2.T
+
             #testing our model using the validation set every 30 epochs
             if(i%30==0):    
                 X_val=X_val.reshape((-1,28*28))
-                val_out=np.argmax(softmax(sigmoid(X_val.dot(l1)).dot(l2)),axis=1)
+                val_out=np.argmax(self.softmax(self.sigmoid(X_val.dot(self.weights[0].T)).dot(self.weights[1].T)),axis=1)
                 val_acc=(val_out==Y_val).mean()
                 self.val_accuracies.append(val_acc.item())
-            if(i%1000==0): 
+            if(i%500==0): 
                 print(f'For {i}th epoch: train accuracy: {accuracy:.3f}| validation accuracy:{val_acc:.3f}')
 
-    def predict(self, a):
-        for w, b in zip(self.weights, self.biases):
-            z = np.matmul(w, a) + b
-            print(z[0])
-            a = self.activation(np.matmul(w, a) + b)
-        return a
+    def test(self, X_test, Y_test):
+        test_out=np.argmax(self.softmax(self.sigmoid(X_test.dot(self.weights[0])).dot(self.weights[1])),axis=1)
+        test_acc=(test_out==Y_test).mean().item()
+        print(f'Test accuracy = {test_acc:.4f}')
+        np.savez('weights',self.weights[0],self.weights[1])
 
     def print_accuracy(self, images, labels):
-        predictions = self.predict(images)
-        num_correct = sum([np.argmax(a) == np.argmax(b)
-                          for a, b in zip(predictions, labels)])
-        print('{0}/{1} accuracy:{2}%'.format(num_correct, len(images),))
-
-    @staticmethod
-    def activation(x):
-        return 1/(1-np.exp(-x))
+        plt.ylim(-0.1, 1.1)
+        plot(self.accuracies)
+        plot(self.val_accuracies)
